b141270fabec107dc566123c9b0aa6116de3a39b
Patryk <salon.skierkowski@gmail.com>
2023-03-21 08:14:29 +0100

    working on contact us panel

--- bongs/src/Components/ContactUs/ContactUs.js
+++ bongs/src/Components/ContactUs/ContactUs.js
@@ -3,25 +3,51 @@ import styles from './ContactUs.module.css'
 import SmartphoneIcon from '@mui/icons-material/Smartphone';
 import EmailIcon from '@mui/icons-material/Email';
 import RoomIcon from '@mui/icons-material/Room';
+import BaseProvider from '../../Context/BaseContext.js';
+import { useContext } from 'react';
 
 export default function ContactUs(content) {
+    const base_ctx = useContext(BaseProvider);
+    const width = base_ctx.screenWidth;
     return(
-        <div className={styles.Container}> 
+        <>
+        {width > 550?
+        <div className={styles.Container}>
+        <span className={styles.Text}>{content.content.ContactUs}</span>
+        <div className={styles.Information}>
+            <div className={styles.SmallContainer}>
+                <SmartphoneIcon fontSize="large"/>
+            <span className={styles.InfText}>+48 600 209 311</span>
+            </div>
+            <div className={styles.SmallContainer}>
+                <EmailIcon fontSize="large"/>
+            <span className={styles.InfText}>a.jachimowski@bongs-segregatory.pl</span>
+            </div>
+            <div className={styles.SmallContainer}>
+                <RoomIcon fontSize="large"/>
+            <span className={styles.InfText}>84-217 Szemud k. Wejherowa Mercerstwo 2</span>
+            </div>
+            </div>
+            </div>
+            :
+            <div className={styles.Container}>
             <span className={styles.Text}>{content.content.ContactUs}</span>
             <div className={styles.Information}>
-                <div className={styles.SmallContainer}>
-                    <SmartphoneIcon fontSize="large"/>
-                <span className={styles.InfText}>+48 600 209 311</span>
-                </div>
-                <div className={styles.SmallContainer}>
-                    <EmailIcon fontSize="large"/>
-                <span className={styles.InfText}>a.jachimowski@bongs-segregatory.pl</span>
-                </div>
-                <div className={styles.SmallContainer}>
-                    <RoomIcon fontSize="large"/>
-                <span className={styles.InfText}>84-217 Szemud k. Wejherowa Mercerstwo 2</span>
-                </div>
+            <div className={styles.SmallContainer}>
+                <SmartphoneIcon fontSize="medium"/>
+            <span className={styles.InfText}>+48 600 209 311</span>
             </div>
-        </div>
+            <div className={styles.SmallContainer}>
+                <EmailIcon fontSize="medium"/>
+            <span className={styles.InfText}>a.jachimowski@bongs-segregatory.pl</span>
+            </div>
+            <div className={styles.SmallContainer}>
+                <RoomIcon fontSize="medium"/>
+            <span className={styles.InfText}>84-217 Szemud k. Wejherowa Mercerstwo 2</span>
+            </div>
+            </div>
+            </div>
+         }
+         </>
     )
 }
--- bongs/src/Components/SectionHeading/SectionHeading.js
+++ bongs/src/Components/SectionHeading/SectionHeading.js
@@ -1,13 +1,9 @@
 import { Box } from "@mui/material";
 import React from "react";
 import styles from './SectionHeading.module.css'
-import { useContext } from "react";
-
 export default function Sectionheading(content){
-    const base_ctx = useContext(BaseProvider);
-    const width = base_ctx.width;
     return(
-        <Box sx={{height: width > 900?  '40vh' : '30vh', width: 'auto'}}>
+        <Box sx={{height: 'auto', width: 'auto'}}>
             <div className={styles.Photo}>
                 <div className={styles.Container}>
                 <span className={styles.H1}>{content.content.Header}</span>
