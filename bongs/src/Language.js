d6d899fe559c37ede7a0155f27116f6e15779902
Patryk <salon.skierkowski@gmail.com>
2023-03-28 14:26:04 +0200

    changing offer panel

--- bongs/src/Language.js
+++ bongs/src/Language.js
@@ -1,7 +1,52 @@
+import packaging1 from './images/Opakowania/opak1.png'
+import packaging2 from './images/Opakowania/opak01.jpg'
+import packaging3 from './images/Opakowania/opak03.jpg'
+
+import binder1 from './images/Segregatory/segregator.jpg'
+import binder2 from './images/Segregatory/segregator2.jpg'
+import binder3 from './images/Segregatory/segregator3.jpg'
+
+import box1 from './images/Pudelka/pud05.jpg'
+import box2 from './images/Pudelka/opak04.jpg'
+import box3 from './images/Pudelka/pud11.jpg'
+
+import sito from './images/Sitodruk/indy04.jpg'
+import sito2 from './images/Sitodruk/sito06.jpg'
+
+import menu from './images/Menu/menu02.jpg'
+import menu2 from './images/Menu/menu03.jpg'
+
+import tlo from './images/Tloczenia/typ_druku.png'
+
+import wiesz from './images/Wieszaki/wiesz_01.jpg'
+import wiesz2 from './images/Wieszaki/wiesz_02.jpg'
+import wiesz3 from './images/Wieszaki/wieszaki.png'
+
+import torba from './images/Torby/torba4.jpg'
+import torba2 from './images/Torby/torba1.jpg'
+import torba3 from './images/Torby/torba12.jpg'
+
+import wkladka from './images/Wkladki/przek01.jpg'
+import wkladka2 from './images/Wkladki/wklad04.jpg'
+
+import wzor from './images/Wzornik/indy08.jpg'
+import wzornik2 from './images/Wzornik/wzor2.jpg'
+import wzornik3 from './images/Wzornik/wzornik02.jpg'
+
+import teczka from './images/Teczki/main_teczka.png'
+import teczka2 from './images/Teczki/tecz10s.jpg'
+
+import etui from './images/Etui/etui2.jpg'
+import etui2 from './images/Etui/IMG_5571.jpg'
+
+import gra from './images/Gry/gra_main.png'
+import gra2 from './images/Gry/gra2_main.png'
+
+
 const Language = {
     Polish : {
         Products: 'Produkty',
-        Calculation: 'Kalkulacje',
+        Calculation: 'Oferta',
         Contact: 'Kontakt',
         Header: 'Opakowania zaprojektowane specjalnie dla Ciebie',
         Description: 'Dazymy do tego, aby kazdy nasz produkt byl wykonany z najlepszych materialow oraz spelnil oczekiwania naszych klientow. Nasze opakowania sa funkcjonalne i estetyczne, co pozwala na skuteczna ochrone produktow oraz wprowadzenie ich w odpowiednia oprawe. Wysoka jakosc naszych wyrobow oraz dbalosc o szczegoly to nasze priorytety, dzieki którym zyskujemy zaufanie klientow i zadowolenie z naszych uslug.',
@@ -9,13 +54,70 @@ const Language = {
         ContactUs: 'Napisz do nas !',
         ThirdHeader: 'Oferta',
         Phone: '+48 600 209 311',
-        Email: 'a.jachimowski@bongs-segregatory.pl',
-        HeaderPhone: 'Twoja marka zasluguje na najlepsze. Wybierz opakowania z naszej oferty'
+        Email: 'aj@pbpolska.com',
+        HeaderPhone: 'Twoja marka zasluguje na najlepsze. Wybierz opakowania z naszej oferty',
+        Cookie: 'Niniejsza strona internetowa korzysta z narzedzia Google Analytics, ktore umozliwia zbieranie informacji na temat sposobu korzystania ze strony przez uzytkownikow. Google Analytics wykorzystuje pliki cookie, czyli niewielkie pliki tekstowe zapisywane na urzadzeniu uzytkownika, ktore umozliwiaja analize korzystania ze strony internetowej. Informacje generowane przez pliki cookie dotyczace korzystania ze strony internetowej (w tym takze adres IP uzytkownika) sa przesylane do serwerow Google i tam przechowywane. Google wykorzystuje te informacje w celu oceny korzystania ze strony internetowej oraz tworzenia raportow dotyczacych aktywnosci na stronie internetowej. Google moze rowniez udostepniac te informacje osobom trzecim, jezeli wymaga tego prawo lub gdy osoby te przetwarzaja dane w imieniu Google.',
+        Opinion: "Jestem klientem od wielu lat i chciałbym polecić tę firmę, ponieważ działają i dostarczają rzetelnie iz wysoką jakością.",
+        NameCompany1: "Agencja Ceraad",
+        Types: [
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Segregator',
+            Images: [binder1,binder2,binder3]
+            },
+            {Type:'Pudełka',
+            Images: [box1,box2,box3]
+            },
+            {Type:'Sitodruk',
+            Images: [sito2,sito]
+            },
+            {Type:'Menu',
+            Images: [menu,menu2]
+            },
+            {Type:'Tłoczenia',
+            Images: [tlo]
+            },
+            {Type:'Wieszaki',
+            Images: [wiesz3,wiesz2,wiesz]
+            },
+            {Type:'Torby',
+            Images: [torba,torba2,torba3]
+            },
+            {Type:'Wzornik',
+            Images: [wzor,wzornik2,wzornik3]
+            },
+            {Type:'Wkładki',
+            Images: [wkladka,wkladka2]
+            },
+            {Type:'Teczki',
+            Images: [teczka,teczka2]
+            },
+            {Type:'Etui',
+            Images: [etui, etui2]
+            },
+            {Type:'Gry',
+            Images: [gra,gra2]
+            },
+            {Type:'Tapetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Zamówienia specjalne',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tapetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            }, 
+        ],
+        ClickToSeeMore: 'Kliknij aby zobaczyć więcej'
+
+        
+
     } ,
     
     English : {
         Products: 'Products',
-        Calculation: 'Calculation',
+        Calculation: 'Offer',
         Contact: 'Contact',
         Header: 'Packaging designed especially for you',
         Description: 'Our company specializes in producing high-quality packaging and folders. We strive to ensure that each of our products is made from the best materials and meets our customers expectations. Our packaging is functional and aesthetic, allowing for effective protection of products and their appropriate presentation. High quality of our products and attention to detail are our priorities, thanks to which we gain the trust of our customers and achieve their satisfaction with our services.',
@@ -24,12 +126,75 @@ const Language = {
         ThirdHeader: 'Offer',
         Phone: '+48 600 209 313',
         Email: 'rl@pbpolska.com',
-        HeaderPhone: 'Your brand deserves the best. Choose packaging from our offer'
+        HeaderPhone: 'Your brand deserves the best. Choose packaging from our offer',
+        Cookie: "This website uses Google Analytics, which enables the collection of information about how users interact with the site. Google Analytics uses cookies, which are small text files stored on the user's device, to analyze website usage. The information generated by the cookie about the use of the website (including the user's IP address) is transmitted to Google servers and stored there. Google uses this information to evaluate website usage and to create reports on website activity. Google may also share this information with third parties if required by law or if these parties process data on behalf of Google.",
+        Opinion: "I have been a customer for many years and would like to recommend this company as they work and deliver reliably and with high quality. That's why I would like to give this company 5 stars.",
+        NameCompany1: "Agency Ceraad",
+        Types: [
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Segregator',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Pudełka',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Sitodruk',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tłoczenia',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wieszaki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Torby',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wkładki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wzornik',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Teczki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Etui',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gry',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gazetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Zamówienia specjalne',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tapetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            }, 
+        ],
+        ClickToSeeMore: 'Kliknij aby zobaczyć więcej'
     },
     
     Swedish : {
         Products: 'Produkter',
-        Calculation: 'Beräkningar',
+        Calculation: 'Erbjudande',
         Contact: 'Kontakt',
         Header: 'Förpackningar designade speciellt för dig',
         Description: 'Vårt företag är specialiserat på tillverkning av högkvalitativa förpackningar och mappar. Vi strävar efter att se till att varje av våra produkter är tillverkad av de bästa materialen och uppfyller våra kunders förväntningar. Våra förpackningar är funktionella och estetiska, vilket möjliggör effektivt skydd av produkter och deras lämpliga presentation. Hög kvalitet på våra produkter och uppmärksamhet på detaljer är våra prioriteringar, tack vare vilka vi vinner förtroendet hos våra kunder och uppnår deras nöjdhet med våra tjänster.',
@@ -38,11 +203,72 @@ const Language = {
         Phone: '+48 600 209 313',
         Email: 'rl@pbpolska.com',
         ThirdHeader: 'Erbjudande',
-        HeaderPhone: 'Ditt varumärke förtjänar det bästa. Välj förpackning från vårt utbud.'
+        HeaderPhone: 'Ditt varumärke förtjänar det bästa. Välj förpackning från vårt utbud.',
+        Cookie: "Den här webbplatsen använder Google Analytics, som möjliggör insamling av information om hur användare interagerar med webbplatsen. Google Analytics använder cookies, vilket är små textfiler som lagras på användarens enhet, för att analysera webbplatsens användning. Informationen som genereras av cookien om användningen av webbplatsen (inklusive användarens IP-adress) överförs till Google-servrar och lagras där. Google använder denna information för att utvärdera webbplatsanvändningen och skapa rapporter om webbplatsaktivitet. Google kan också dela denna information med tredje parter om det krävs enligt lag eller om dessa parter behandlar data på uppdrag av Google.",
+        Types: [
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Segregator',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Pudełka',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Sitodruk',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tłoczenia',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wieszaki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Torby',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wkładki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wzornik',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Teczki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Etui',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gry',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gazetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Zamówienia specjalne',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tapetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            }, 
+        ],
+        ClickToSeeMore: 'Kliknij aby zobaczyć więcej'
     },
     German : {
         Products: 'Produkte',
-        Calculation: 'Berechnung',
+        Calculation: 'Angebot',
         Contact: 'Kontakt',
         Header: 'Verpackungen speziell für Sie entworfen',
         Description: 'Unser Unternehmen ist auf die Herstellung von Verpackungen und Ordnern höchster Qualität spezialisiert. Wir streben danach, dass jedes unserer Produkte aus den besten Materialien hergestellt wird und den Erwartungen unserer Kunden entspricht. Unsere Verpackungen sind funktional und ästhetisch, was einen effektiven Schutz der Produkte und ihre angemessene Präsentation ermöglicht. Hohe Qualität unserer Produkte und Liebe zum Detail sind unsere Prioritäten, dank denen wir das Vertrauen unserer Kunden gewinnen und ihre Zufriedenheit mit unseren Dienstleistungen erreichen.',
@@ -51,7 +277,70 @@ const Language = {
         Phone: '+48 600 209 313',
         Email: 'rl@pbpolska.com',
         ContactUs: 'Kontaktieren Sie uns !',
-        HeaderPhone: 'Ihre Marke verdient das Beste. Wählen Sie die Verpackung aus unserem Angebot.'
+        HeaderPhone: 'Ihre Marke verdient das Beste. Wählen Sie die Verpackung aus unserem Angebot.',
+        Cookie: "Diese Website verwendet Google Analytics, das die Erfassung von Informationen darüber ermöglicht, wie Benutzer mit der Website interagieren. Google Analytics verwendet Cookies, die kleine Textdateien sind, die auf dem Gerät des Benutzers gespeichert werden, um die Nutzung der Website zu analysieren. Die durch das Cookie generierten Informationen über die Nutzung der Website (einschließlich der IP-Adresse des Benutzers) werden an Google-Server übertragen und dort gespeichert. Google verwendet diese Informationen zur Auswertung der Website-Nutz",
+        Opinion: "Schon viele Jahre bin ich Kunde und möchte diese Firma sehr gerne weiterempfehlen, da zuverlässig und in hoher Qualität gearbeitet und geliefert wird. Dieses Unternehmen möchte ich daher mit 5 Sternen bewerten.",
+        NameCompany1: "CeraAnzeigenagentur",
+        Types: [
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Opakowania',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Segregator',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Pudełka',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Sitodruk',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tłoczenia',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wieszaki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Menu',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Torby',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wkładki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Wzornik',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Teczki',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Etui',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gry',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Gazetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Zamówienia specjalne',
+            Images: [packaging1,packaging2,packaging3]
+            },
+            {Type:'Tapetowiec',
+            Images: [packaging1,packaging2,packaging3]
+            }, 
+        ],
+        ClickToSeeMore: 'Kliknij aby zobaczyć więcej'
     }
 }
 
